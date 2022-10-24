b0de0b9af1ee26c68d091305c449ef040c01975b
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2022-10-24 12:30:52 +0530

    Add resize method to dynamic array

--- arrays/dynamic.py
+++ arrays/dynamic.py
@@ -4,6 +4,13 @@ class Dynamic_Array:
         self._capacity = 1
         self._Array = self._create(self._capacity)
 
+
     def _create(self,capacity):
         return [None] * capacity
 
+    def _resize(self, capacity):
+        NewArray = [None] * capacity
+        for i in range(self._size):
+            NewArray[i] = self._Array[i]
+        self._Array = NewArray
+        self._capacity = capacity
