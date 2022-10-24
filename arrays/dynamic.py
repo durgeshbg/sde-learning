4070254ba058e030cfe81888e42332c690a74009
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2022-10-24 21:18:47 +0530

    modify dynamic.py

--- arrays/dynamic.py
+++ arrays/dynamic.py
@@ -2,16 +2,32 @@ class Dynamic_Array:
     def __init__(self):
         self._size = 0
         self._capacity = 1
-        self._Array = self._create(self._capacity)
+        self._Array = [None] * self._capacity
 
-    def get(self, i):
+    def size(self):
+        return self._size
+    
+    def capacity(self):
+        return self._capacity
+
+    def is_empty(self):
+        if self._size == 0:
+            return True
+        return False
+
+    def at(self, i):
         if i < 0 or i >= self._capacity:
             raise IndexError("Out of range.")
         return self._Array[i]
 
-    def pushback(self, item):
+    def push(self, item):
         if self._capacity == self._size:
-            self._resize(2 * self._capacity)
+            NewArray = [None] * (2 * self._capacity)
+            for i in range(self._size):
+                NewArray[i] = self._Array[i]
+            self._Array = NewArray
+            self._capacity = 2 * self._capacity
+        
         self._Array[self._size] = item
         self._size += 1
 
@@ -22,11 +38,6 @@ class Dynamic_Array:
             self._Array[j] = self._Array[j + 1]
         self._size -= 1
 
-    def size(self):
-        return self._size
-    
-    def capacity(self):
-        return self._capacity
 
     def __str__(self):
         message = ""
@@ -35,17 +46,3 @@ class Dynamic_Array:
             message += " "
         return message
     
-    def is_empty(self):
-        if self._size == 0:
-            return True
-        return False
-
-    def _create(self, capacity):
-        return [None] * capacity
-
-    def _resize(self, capacity):
-        NewArray = [None] * capacity
-        for i in range(self._size):
-            NewArray[i] = self._Array[i]
-        self._Array = NewArray
-        self._capacity = capacity
