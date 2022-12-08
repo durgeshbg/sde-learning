c9181f3bb0777ba507d1ae157ef419d18412cf27
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2022-12-08 19:17:56 +0530

    dynamic array in c

---
+++ arrays/dynamic.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void display(int *);
+int size(int *);
+bool is_empty(int *);
+
+int main(void)
+{
+    int *a, s;
+    printf("Enter no. of elements to store: ");
+    scanf("%d", &s);
+    a = malloc((s+1) * sizeof(int));
+    printf("Storing %d integers...\n",s);
+    for (int i = 0; i < s; i++)
+        *(a + i) = i;
+    *(a + s) = -999;
+    printf("Size: %d | Capacity: %d\n", size(a), s);
+    if (is_empty(a))
+        printf("Empty\n");
+    display(a);
+    
+    return 0;
+}
+
+void display(int *a)
+{
+    int i = 0;
+    printf("Integers:\n");
+    while (*(a+i) != -999) {
+        printf("%d: %d\n",i ,*(a+i));
+        i++;
+    }
+}
+int size(int *a)
+{
+    int i = 0;
+    while (*(a+i) != -999)
+        i++;
+    return i;
+}
+bool is_empty(int *a) {
+    if (size(a) == 0)
+        return true;
+    return false;
+}
