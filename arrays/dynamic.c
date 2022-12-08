c9a651f26f0da6cf396a616b1550d6376d806b5c
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2022-12-08 23:20:35 +0530

    add push function in dynamic.c

--- arrays/dynamic.c
+++ arrays/dynamic.c
@@ -2,36 +2,39 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+int CAPACITY;
 void display(int *);
 int size(int *);
 bool is_empty(int *);
-void at(int *, int, int);
+void at(int *, int);
+int *push(int *, int);
 
 int main(void)
 {
-    int *a, s;
+    int *a;
     printf("Enter no. of elements to store: ");
-    scanf("%d", &s);
-    a = malloc((s + 1) * sizeof(int));
-    printf("Storing %d integers...\n", s);
-    for (int i = 0; i < s; i++)
+    scanf("%d", &CAPACITY);
+    a = malloc((CAPACITY + 1) * sizeof(int));
+    printf("Storing %d integers...\n", CAPACITY);
+    for (int i = 0; i < CAPACITY; i++)
         *(a + i) = i;
-    *(a + s) = -999;
+    *(a + CAPACITY) = -999;
 
-    printf("Size: %d | Capacity: %d\n", size(a), s);
+    printf("Size: %d | Capacity: %d\n", size(a), CAPACITY);
     if (is_empty(a))
-        printf("Empty\n");
+        printf("\nEmpty\n");
     display(a);
 
-    at(a, 7, s);
-
+    at(a, 1);
+    a = push(a, 5);
+    display(a);
     return 0;
 }
 
 void display(int *a)
 {
     int i = 0;
-    printf("Integers:\n");
+    printf("\n----Integers:----\n");
     while (*(a + i) != -999)
     {
         printf("%d: %d\n", i, *(a + i));
@@ -51,10 +54,25 @@ bool is_empty(int *a)
         return true;
     return false;
 }
-void at(int *a, int i, int s)
+void at(int *a, int i)
 {
-    if (i >= s)
-        printf("Index out of bound\n");
+    if (i >= CAPACITY)
+        printf("\nIndex out of bound\n");
     else
-        printf("Returning element at %d: %d\n", i, *(a + i));
+        printf("\n=> Returning element at %d: %d\n", i, *(a + i));
+}
+int *push(int *a, int x)
+{
+    int *t, s, i = 0;
+    s = size(a);
+    t = malloc((s + 2) * sizeof(int));
+    printf("\n+ + + + Pushing %d + + + +\n",x);
+    while (*(a + i) != -999)
+    {
+        *(t + i) = *(a + i);
+        i++;
+    }
+    *(t + s) = x;
+    *(t + s + 1) = -999;
+    return t;
 }
