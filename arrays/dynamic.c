07fafdab757a94040c29c9b9c12e5fd3393a66d5
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2022-12-09 09:57:41 +0530

    add insert function in dynamic.c

--- arrays/dynamic.c
+++ arrays/dynamic.c
@@ -8,6 +8,7 @@ int size(int *);
 bool is_empty(int *);
 void at(int *, int);
 int *push(int *, int);
+int *insert(int *, int, int);
 
 int main(void)
 {
@@ -28,16 +29,19 @@ int main(void)
     at(a, 1);
     a = push(a, 5);
     display(a);
+
+    a = insert(a, 2, 99);
+    display(a);
     return 0;
 }
 
 void display(int *a)
 {
     int i = 0;
-    printf("\n----Integers:----\n");
+    printf("----Integers:----\n");
     while (*(a + i) != -999)
     {
-        printf("%d: %d\n", i, *(a + i));
+        printf("%d: %d\n", i , *(a + i));
         i++;
     }
 }
@@ -59,20 +63,35 @@ void at(int *a, int i)
     if (i >= CAPACITY)
         printf("\nIndex out of bound\n");
     else
-        printf("\n=> Returning element at %d: %d\n", i, *(a + i));
+        printf("\nReturning element at %d: %d\n", i, *(a + i));
 }
 int *push(int *a, int x)
 {
     int *t, s, i = 0;
     s = size(a);
     t = malloc((s + 2) * sizeof(int));
-    printf("\n+ + + + Pushing %d + + + +\n",x);
+    printf("\n+ + + + Pushing %d + + + +\n", x);
     while (*(a + i) != -999)
     {
         *(t + i) = *(a + i);
         i++;
     }
     *(t + s) = x;
+    *(t + s + 1) = -999;
+    return t;
+}
+int *insert(int *a, int i, int x)
+{
+    int *t, s, j = 0;
+    s = size(a);
+    printf("\n* * * Inserting %d at %d * * *\n", x, i);
+    t = malloc((s + 2) * sizeof(int));
+    while (*a != -999)
+    {
+        *(t + j) = i == j ? x : *(a++);
+        j++;
+    }
+    printf("%d\n",j);
     *(t + s + 1) = -999;
     return t;
 }
