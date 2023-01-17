3662a284e0193f5ff8b79e125887067e293d8eac
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-01-17 17:36:49 +0530

    Add PushFront function

--- linked-lists/linked-list.c
+++ linked-lists/linked-list.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct Node
+typedef struct Node
 {
     int data;
-    struct Node * next;
-};
+    struct Node *next;
+} Node;
+
+Node *head, *tail;
+
+Node *PushFront(int);
 
 int main(void)
 {
     return 0;
+}
+
+Node *PushFront(int key)
+{
+    Node *node = (Node *) malloc(sizeof(Node));
+    node->data = key;
+    node->next = head;
+    head = node;
+    if (tail == NULL)
+        tail = head;
 }
