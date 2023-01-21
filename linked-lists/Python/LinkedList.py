4a97f5949e24fcafd7dc5f55031c0a8eb799a75c
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-01-22 01:01:38 +0530

    create node class in LinkedList.py

--- linked-lists/Python/LinkedList.py
+++ linked-lists/Python/LinkedList.py
@@ -1,5 +1,15 @@
 class Node:
-    ...
+    def __init__(self, data, next):
+        self.data = data
+        self.next = None
     
+    @property
+    def data(self):
+        return self._data
+    @data.setter
+    def data(self, data):
+        self._data = data
+    
+
 class LinkedList:
     ...
